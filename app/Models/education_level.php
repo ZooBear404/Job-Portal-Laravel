1ecf8964509fd8e4865865c37d1b547c65a56f68
ZooBear404 <zubairstudytech@gmail.com>
2024-12-18 14:12:53 +0430

    feat(orm): create education level model
    * model
    * migration

---
+++ app/Models/education_level.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class education_level extends Model
+{
+    /** @use HasFactory<\Database\Factories\EducationLevelFactory> */
+    use HasFactory;
+
+    protected $table = "education_level";
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'education_level_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'name',
+        'rank_value'
+    ];
+
+    protected $casts = [
+        'rank_value' => 'int'
+    ];
+}
