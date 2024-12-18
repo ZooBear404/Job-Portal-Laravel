bc8c184eb7de9a921a104ef2c2e16a7153e4e76d
ZooBear404 <zubairstudytech@gmail.com>
2024-12-18 11:10:39 +0430

    feat(orm): create admin's model
    * model
    * migration
    * policy
    * factory
    * seeder

---
+++ app/Models/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class admin extends Model
+{
+    /** @use HasFactory<\Database\Factories\AdminFactory> */
+    use HasFactory;
+
+    protected $table = "admin";
+    protected $primaryKey = "admin_id";
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected function casts(): array {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed'
+        ];
+    }
+
+}
