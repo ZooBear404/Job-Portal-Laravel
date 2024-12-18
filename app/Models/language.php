49bb9d22dc9cc8da86dc67a322363f31332c5ff7
ZooBear404 <zubairstudytech@gmail.com>
2024-12-18 14:18:33 +0430

    feat(orm): create language model

---
+++ app/Models/language.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class language extends Model
+{
+    /** @use HasFactory<\Database\Factories\LanguageFactory> */
+    use HasFactory;
+
+    protected $table = "language";
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'language_id';
+    public $incrementhing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'name',
+    ];
+}
