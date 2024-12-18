dd69355899e60783c4c77bcc98d5ef5f2c94e7d3
ZooBear404 <zubairstudytech@gmail.com>
2024-12-18 14:16:17 +0430

    feat(orm): create skill model

---
+++ app/Models/skill.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class skill extends Model
+{
+    /** @use HasFactory<\Database\Factories\SkillFactory> */
+    use HasFactory;
+
+    protected $table = "skill";
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'skill_id';
+    public $incrementhing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'name',
+    ];
+}
