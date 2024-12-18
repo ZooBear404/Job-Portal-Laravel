c8c25804abe936f17fc15d4e4f6c2141a8b97d1b
ZooBear404 <zubairstudytech@gmail.com>
2024-12-18 13:46:24 +0430

    feat(orm): create institute's model
    * model
    * migration

---
+++ app/Models/institute.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class institute extends Model
+{
+    /** @use HasFactory<\Database\Factories\InstituteFactory> */
+    use HasFactory;
+
+        //
+    protected $table = "institute";
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'institute_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'name',
+    ];
+}
